d2de9f47b80231aaa866994e9e5a9df1586a1df7
woshinikaikaige <936523016@qq.com>
2018-04-27 17:42:42 +0800

    serialize and deserialize
    serialize and deserialize from client

--- Server/socket.js
+++ Server/socket.js
@@ -2,54 +2,54 @@ var net = require('net');
 var protobuf = require('protobufjs');
 var customProto = require("./CustomProto/CustomProtocol_pb.js");
 var loginReq = customProto.MsgClientTestLoginReq;
-
+var loginRsp = customProto.MsgClientLoginRsp;
 
 var server = net.createServer(
 function(socket)
 {
 	console.log("a connection...." + socket.remoteAddress + ':' + socket.port);
 	
-	socket.setEncoding("base64");
+	//socket.setEncoding("base64");
 	socket.on('data',function(data){
-		//var i = 0;
-		//console.log("total length:" + data.length);
-		//var length = data.readInt16LE(i);
-		//console.log('a data ..L..' + length);
-		//i++;
-		//i++;
-		//var t = data.readInt16LE(2);
-	    //console.log('a data ..L..' + length + "[t]" + t);
-	    var t = 0;
-		if(t === 9)
+		var dataBuffer = Buffer.from(data);
+		var i = 0;
+		console.log("data type:" + typeof(data));
+		console.log("total data length:" + data.length);
+		console.log("total dataBuffer length:" + dataBuffer.length);
+		//message length 2+4+message.length
+		var length = data.readInt16LE(i);
+		i++;
+		i++;
+		//message type 
+		var t = dataBuffer.readUInt16LE(i);
+	    console.log('a data [Length]' + length + "[type]" + t);
+		if(t === 1)
 		{
+			//use protobufjs to deserialize
+			var encodeBuffer = data.slice(8,length - 6 + 8);
+			console.log("deserialize length :" + encodeBuffer.length + typeof(encodeBuffer));
+			var u8array = new Uint8Array(encodeBuffer);
+			var ack = loginReq.deserializeBinary(u8array);
+			console.log("ack account :" + ack.getAccount());
+			console.log("ack passwd :" + ack.getPasswd());
 			
-			var message = heartBeat.create(); // or use .fromObject if conversion is necessary
-            var encodeBuffer = heartBeat.encode(message).finish();
-			var lengthBuffer = Buffer.alloc(2);
-			lengthBuffer.writeInt16LE(encodeBuffer.length + 6);
-			var typeBuffer = Buffer.alloc(2);
-			typeBuffer.writeInt16LE(0);
-			var externBuffer = Buffer.alloc(4);
-			var sendBuffer = Buffer.concat([lengthBuffer,typeBuffer,externBuffer,encodeBuffer]);
+			//use protobufjs to serialize and send message
+			var rsp = new loginRsp();
+			rsp.setUserId(936523016);
+			rsp.setToken("tttttt");
+			rsp.setGateIp("127.0.0.1");
+			rsp.setGatePort(3002);
+			var b = rsp.serializeBinary();
+			var headBuffer = Buffer.alloc(8);
+			headBuffer.writeInt16LE(b.length + 6);
+			headBuffer.writeUInt16LE(11,2);
+			headBuffer.writeInt32LE(0,4);
+			sendBuffer = Buffer.concat([headBuffer,b],headBuffer.length + b.length);
+			//send to client
 			socket.write(sendBuffer);
-		}
-		else if(t === 1)
-		{
 			
-			
-			var length = data.readInt16LE();
-			console.log("length" + length);
-			var encodeBuffer = Buffer.alloc(length).fill(data, 8, length - 6,"binary");
-			var ui8array = new Uint8Array(encodeBuffer);
-			var ack = loginReq.deserializeBinary(encodeBuffer);
-			console.log("ack" + ack.gate_ip);
-			console.log("ack" + ack.gateIp);
-
-			socket.write(sendBuffer);
 		}
-		var ack = loginReq.deserializeBinary(data);
-		console.log("ack a" + ack.getAccount());
-		console.log("ack p" + ack.getPasswd());
+		
 	});
 
 	
@@ -80,7 +80,7 @@ server.on("error",function(exception){
 
 
 var req = new loginReq();
-req.setAccount("zzz");
+req.setAccount("myAccount");
 req.setPasswd("123");
 var b = req.serializeBinary();
 var dreq = loginReq.deserializeBinary(b);
